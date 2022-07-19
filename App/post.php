367c7640cd9d3b95a2a533ce6eed2213e727894e
Hampni <magnus9044@gmail.com>
2022-07-19 18:13:11 +0300

    1 update

--- App/post.php
+++ App/post.php
@@ -11,9 +11,8 @@ try {
 
 
 $db = new \App\Db();
-$sql = "INSERT INTO User (first_name, last_name , birthday, report_subject, country, phone, email)
-VALUES ('Illia','Prodanets','2001-08-12','marine navigation', 'Ukraine', '+380957430387', 'magnus9044@gmail.com')";
-
+//$sql = "INSERT INTO User (first_name, last_name , birthday, report_subject, country, phone, email)
+//VALUES ('Illia','Prodanets','2001-08-12','marine navigation', 'Ukraine', '+380957430387', 'magnus9044@gmail.com')";
 //$db->query($sql);
 
 echo '<h1 style="font-size: 30px;">Данные успешно сохранены</h1>';
